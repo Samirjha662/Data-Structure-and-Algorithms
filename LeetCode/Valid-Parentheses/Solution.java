31c6e908cefc70cd50d1bc0c96b4d5988c9def02
Samirjha662 <samirjha662@gmail.com>
2026-08-05 16:36:58 +0530

    Add solution for Valid Parentheses

--- LeetCode/Valid-Parentheses/Solution.java
+++ LeetCode/Valid-Parentheses/Solution.java
@@ -1,29 +1,54 @@
 1class Solution {
-2    public boolean isValid(String s) {
-3
-4        Stack<Character> st = new Stack();
+2    public boolean isValid(String s ){
+3        char [] ch = new char[s.length()];
+4        int top =0;
 5
-6        for(char ch : s.toCharArray()){
-7
-8            if(ch =='(' || ch =='[' || ch =='{'){
-9                st.push(ch);
-10            }
-11            else{
-12                 if(st.isEmpty()){
-13                    return false;
-14                 }
-15             if((ch==')' && st.peek()!='(') || 
-16               (ch==']' && st.peek()!='[') || 
-17               (ch=='}' && st.peek()!='{')) {
-18                return false;   
-19            }
-20             
-21            st.pop();
-22            }
+6        for(char c :s.toCharArray()){
+7        switch(c){
+8            case '(':
+9                 ch[top++] =')';
+10                 break;
+11            case '[':
+12                 ch[top++] =']';
+13                 break;
+14            case '{':
+15                 ch[top++] ='}';
+16                 break;
+17            default:
+18                   if(top==0 || ch[--top] !=c){
+19                    return false;
+20                   }
+21        }
+22        }
 23
-24            
+24            return top==0;
 25        }
-26       
-27        return st.isEmpty();    
-28    }
-29}
+26    }
+27    // public boolean isValid(String s) {
+28
+29    //     Stack<Character> st = new Stack();
+30
+31    //     for(char ch : s.toCharArray()){
+32
+33    //         if(ch =='(' || ch =='[' || ch =='{'){
+34    //             st.push(ch);
+35    //         }
+36    //         else{
+37    //              if(st.isEmpty()){
+38    //                 return false;
+39    //              }
+40    //          if((ch==')' && st.peek()!='(') || 
+41    //            (ch==']' && st.peek()!='[') || 
+42    //            (ch=='}' && st.peek()!='{')) {
+43    //             return false;   
+44    //         }
+45             
+46    //         st.pop();
+47    //         }
+48
+49            
+50    //     }
+51       
+52    //     return st.isEmpty();    
+53    // }
+54// }
