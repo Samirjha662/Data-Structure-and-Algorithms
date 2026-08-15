01a37b55d8f72b22b6db0ea29dc7b013b359ed57
Samirjha662 <samirjha662@gmail.com>
2026-08-16 00:56:14 +0530

    Add solution for Longest Subsequence With Non-Zero Bitwise XOR

--- LeetCode/Longest-Subsequence-With-Non-Zero-Bitwise-XOR/Solution.java
+++ LeetCode/Longest-Subsequence-With-Non-Zero-Bitwise-XOR/Solution.java
@@ -2,18 +2,18 @@
 2    public int longestSubsequence(int[] nums) {
 3       
 4        int xor =0;
-5        boolean nonZero =false;
+5       int nonZero =0;
 6        for(int i =0; i<nums.length ;i++){
 7            xor = xor ^ nums[i];
 8            if(nums[i]!=0){
-9                nonZero = true;
+9                nonZero++;
 10            } 
 11
 12        }
 13        if(xor!=0){
 14            return nums.length;
 15        }
-16        if(nonZero){
+16        if(nonZero!=0){
 17            return nums.length-1;
 18        }
 19        return 0;
