0008aa7fffb50901040b9d8e7b4815128a65ad71
Samirjha662 <samirjha662@gmail.com>
2026-08-25 15:12:30 +0530

    Time: 1 ms (82.00%) | Memory: 44.7 MB (95.40%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.java
@@ -1,22 +1,37 @@
 class Solution {
-    public int missingMultiple(int[] nums, int k) {
-        HashMap<Integer ,Integer> map = new HashMap<>();
-        int max = 0;
-
-        for(int i =0; i<nums.length ;i++){
-            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
-             max = Math.max(nums[i],max);
+     public int missingMultiple(int[] nums, int k) {
+        for(int multiple=k;;multiple+=k){
+            boolean found=false;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]==multiple){
+                    found=true;
+                    break;
+                }
+            }
+            if(!found){
+            return multiple;
+            }
         }
-      
-
-        for(int i =1; i<=max ;i++){
-             int ans = k *i;
-             if(map.containsKey(ans)){
-                continue;
-             }else return ans;
-
-        }
-        return max +k;
         
     }
+    // public int missingMultiple(int[] nums, int k) {
+    //     HashMap<Integer ,Integer> map = new HashMap<>();
+    //     int max = 0;
+
+    //     for(int i =0; i<nums.length ;i++){
+    //         map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+    //          max = Math.max(nums[i],max);
+    //     }
+      
+
+    //     for(int i =1; i<=max ;i++){
+    //          int ans = k *i;
+    //          if(map.containsKey(ans)){
+    //             continue;
+    //          }else return ans;
+
+    //     }
+    //     return max +k;
+        
+    // }
 }
