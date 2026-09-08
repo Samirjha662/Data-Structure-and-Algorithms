746ac49316cea953b4478667eaa7d16d9fc9d12d
Samirjha662 <samirjha662@gmail.com>
2026-09-08 16:00:16 +0530

    Add solution for Subarray Sum Equals K

--- LeetCode/Subarray-Sum-Equals-K/Solution.java
+++ LeetCode/Subarray-Sum-Equals-K/Solution.java
@@ -1,32 +1,35 @@
 1class Solution {
-2    //  public int subarraySum(int[] nums, int k) {
-3    //    HashMap<Integer,Integer> map = new HashMap<>();
-4    //    int prefixSum=0;
-5    //    int cnt = 0;
-6        
-7    //     for(int i = 0 ;i< nums.length; i++){
-8    //         prefix+= nums[i];
-9            
-10    //         if(map.containsKey(k - prefixSum)){
-11    //             cnt+= map.get(k-prefixSum);
-12    //         }
-13    //         map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-14           
-15    //     }
-16       
-17    //     return cnt;
-18    // }
-19    public int subarraySum(int[] nums, int k) {
-20        int count =0;
-21        
-22        for(int i = 0 ;i< nums.length; i++){
-23            int sum = nums[i];
-24            if(sum ==k) count++;
-25            for( int j = i+1; j< nums.length ;j++){
-26               sum += nums[j];
-27               if(sum== k) count++;
-28            }
-29        }
-30        return count;
-31    }
-32}
+2     public int subarraySum(int[] nums, int k) {
+3       HashMap<Integer,Integer> map = new HashMap<>();
+4       map.put(0,1);
+5
+6
+7       int prefixSum=0;
+8       int cnt = 0;
+9        
+10        for(int i = 0 ;i< nums.length; i++){
+11            prefixSum += nums[i];
+12            
+13            if(map.containsKey(prefixSum-k)){
+14                cnt+= map.get(prefixSum-k);
+15            }
+16            map.put(prefixSum,map.getOrDefault(prefixSum,0)+1);
+17           
+18        }
+19       
+20        return cnt;
+21    }
+22    // public int subarraySum(int[] nums, int k) {
+23    //     int count =0;
+24        
+25    //     for(int i = 0 ;i< nums.length; i++){
+26    //         int sum = nums[i];
+27    //         if(sum ==k) count++;
+28    //         for( int j = i+1; j< nums.length ;j++){
+29    //            sum += nums[j];
+30    //            if(sum== k) count++;
+31    //         }
+32    //     }
+33    //     return count;
+34    // }
+35}
