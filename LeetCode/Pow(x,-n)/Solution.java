c839626886509cc7a776cea8510370db64b83975
Samirjha662 <samirjha662@gmail.com>
2026-09-14 02:09:17 +0530

    Add solution for Pow(x, n)

--- LeetCode/Pow(x,-n)/Solution.java
+++ LeetCode/Pow(x,-n)/Solution.java
@@ -1,23 +1,48 @@
 1class Solution {
-2    public double myPow(double x, int n) {
-3            long power =n;
-4            if(power<0){
-5                x=1/x;
-6                power= -power;
-7            }
-8            double ans = 1;
+2
+3    public double myPow(double x, int n){
+4        long power = n;
+5
+6        if(power <0){
+7            x = 1/x;
+8            power = -power;
 9
-10            while(power> 0){
-11                if(power %2 ==1){
-12                    ans *= x;
-13                }
-14                x*=x;
-15                power /=2;
-16
-17            }
-18
+10        }
+11
+12        return power(x,power);
+13    }
+14
+15    private double power(double x, long n){
+16        if(n==0){
+17            return 1;
+18        }
 19
-20            return ans;
-21
-22    }
-23}
+20        double half = power(x,n/2);
+21        if(n%2 ==0){
+22           
+23            return half* half;
+24        }
+25        return half * half *x;
+26    }
+27    // public double myPow(double x, int n) {
+28    //         long power =n;
+29    //         if(power<0){
+30    //             x=1/x;
+31    //             power= -power;
+32    //         }
+33    //         double ans = 1;
+34
+35    //         while(power> 0){
+36    //             if(power %2 ==1){
+37    //                 ans *= x;
+38    //             }
+39    //             x*=x;
+40    //             power /=2;
+41
+42    //         }
+43
+44
+45    //         return ans;
+46
+47    // }
+48}
