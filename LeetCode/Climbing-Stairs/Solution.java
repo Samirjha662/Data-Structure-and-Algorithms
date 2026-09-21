e84c0a5c044e260daabe54e3aa1ff060f5adcf43
Samirjha662 <samirjha662@gmail.com>
2026-09-21 17:19:48 +0530

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.java
+++ LeetCode/Climbing-Stairs/Solution.java
@@ -2,21 +2,22 @@
 2    private int rec(int n , int[] dp){
 3        if(n==1 || n==0) return 1;
 4        if(dp[n]!=-1) return dp[n];
-5
+5    
 6
-7        return dp[n] = rec(n-1,dp) + rec(n-2,dp);
-8
-9    }
-10   
-11    public int climbStairs(int n) {
-12        int [] dp = new int[n+1];
-13        for(int i =0 ;i<dp.length ;i++){
-14            dp[i]=-1;
-15        }
-16        
+7
+8        return dp[n] = rec(n-1,dp) + rec(n-2,dp);
+9
+10    }
+11   
+12    public int climbStairs(int n) {
+13        int [] dp = new int[n+1];
+14        for(int i =0 ;i<dp.length ;i++){
+15            dp[i]=-1; 
+16        }
 17        
 18        
-19        return rec(n,dp);
-20        
-21    }
-22}
+19        
+20        return rec(n,dp);
+21        
+22    }
+23}
